89ed3f196f3f1f204f8466121346bff9b8065eac
12widasa <widasaputra1@gmail.com>
2022-10-18 11:51:23 +0700

    add script js

---
+++ public/js/script.js
@@ -0,0 +1,106 @@
+// navbar
+let answers = [];
+let currentPage = 1;
+let data = JSON.parse(localStorage.getItem("data"));
+let filterType = [
+    "ALL",
+    "PEMILU",
+    "ORMAS",
+    "DPO",
+    "3 PILAR",
+    "LTC",
+    "UMUM",
+    "PPA",
+    "POLSUS",
+    "BANKOM",
+    "OJOL",
+    "ZEBRA TRAINING CENTER",
+];
+
+const getNavFeeds = () => {
+    fetch(
+        `http://api-feed.pcctabessmg.xyz/api/fd/get_cari_feed_web.php?page=1&keyword=Semarang&type=${filterType}`
+        // `http://api-feed.pcctabessmg.xyz/api/fd/get_cari_feed_web.php?page=1&keyword=Semarang&type=BANKOM`
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((responseJson) => {
+            console.log(responseJson.feed);
+            const Navdata = responseJson.feed;
+            // showListExam(data);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+};
+
+const getAnswer = (value) => {
+    getNavFeeds();
+};
+
+// index
+
+const feeds = document.querySelector("#feeds");
+const getAllFeeds = () => {
+    const linkAllFeeds = `http://api-feed.pcctabessmg.xyz/api/fd/get_all_feed_web.php?page=1&type=BANKOM`;
+
+    fetch(linkAllFeeds)
+        .then((response) => {
+            return response.json();
+        })
+        .then((responseJson) => {
+            const data = responseJson.feed;
+            showFeed(data);
+        })
+        .catch((err) => {
+            console.log(error);
+        });
+};
+
+const showFeed = (Feed) => {
+    Feed.forEach((item) => {
+        var html = createFeed(item);
+        feeds.innerHTML += html;
+    });
+};
+
+const createFeed = (feed) => {
+    const urlContent = "http://api-feed.pcctabessmg.xyz/files/";
+    let avatar = feed.user_detail.avatar
+        ? `https://api.pcctabessmg.xyz/${feed.user_detail.avatar}`
+        : "/assets/images/img_profil_default.png";
+    let content = feed.file
+        ? `<img src="${urlContent}${feed.file}" class="img-content">`
+        : "";
+    if (feed.jenis === "FEED_VIDEO") {
+        content = `<video class="img-content" controls>
+                        <source src="${urlContent}${feed.file}" type="video/mp4">
+                        Your browser does not support HTML video.
+                    </video>`;
+    }
+
+    return `<div class="cards bg-dark-gray text-white">
+                    <div class="d-flex align-items-center ">
+                        <img src="${avatar}" class="logo-avatar">
+                        <div class="d-flex flex-column ms-3">
+                            <span>${feed.user_detail.name}</span>
+                            <span>24 jam yang lalu</span>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="mt-1">${feed.caption}</p>
+                        ${content}
+                        <div class="btn-group-topics">
+                            <button class="btn-topics"><i class="fa-solid fa-heart me-2"></i>${feed.like}</button>
+                            <button class="btn-topics"><i class="fa-solid fa-comment me-2"></i>${feed.comment_count}</button>
+                            <button class="btn-topics"><i class="fa-solid fa-share me-2"></i>Bagikan</button>
+                        </div>
+                    </div>
+                </div>`;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    getAllFeeds();
+    getNavFeeds();
+});
--- resources/views/layouts/includes/navbar-feed-libas.blade.php
+++ resources/views/layouts/includes/navbar-feed-libas.blade.php
@@ -1,4 +1,87 @@
 <section>
+    <style>
+        .navbar>.container-fluid {
+            display: flex;
+            flex-wrap: nowrap;
+            align-items: center;
+            justify-content: space-between;
+        }
+    
+        .global-navbar {
+        /* background-color: #33333366; */
+        background: #1b1b1b;
+        }
+    
+        .btn-group-nav {
+            display: flex;
+            justify-content: center;
+        }
+    
+        .btn-group {
+            gap: 8px;
+            display: flex;
+            overflow-x: auto;
+            padding: 0 12px;
+            margin-top: 1rem;
+        }
+        .btn-nav {
+            border-radius: 10px;
+            min-width: 5.2rem;
+            background-color: #333333;
+            color: white;
+            border: none;
+            padding: 0.5rem 0;
+        }
+        .btn-nav:hover {
+            background-color: #fedf2c;
+        }
+        .btn-outline-success {
+            color: #fedf2c;
+            border: 1px solid #fedf2c;
+        }
+        .btn-outline-success:hover {
+            background-color: #fedf2c;
+        }
+        .btn-outline-success:active {
+            background-color: #fedf2c !important    ;
+        }
+    
+        .img span {
+            margin-left: 1rem;
+            line-height: 20px;
+            font-weight: 600;
+        }
+        .img-responsive {
+            max-width: 5rem; 
+            height: auto;
+    
+        }
+    
+    
+        @media only screen and (max-width: 600px) {
+            .img span {
+                display: none;
+            }
+            .nav-up{
+                padding: 0 1rem;
+            }
+            .topics {
+                padding: 0 10%;
+            }
+        }
+    
+        @media only screen and (min-width: 768px) {
+            .nav-up{
+                padding: 0 2rem ;
+            }
+        }
+    
+        @media only screen and (min-width: 992px) {
+            .nav-up{
+                padding: 0 4rem ;
+            }
+        }
+        </style>
     <div class="global-navbar">
         <nav class="navbar py-3 btn-group-nav" >
             <div class="container-fluid nav-up">
@@ -16,115 +99,43 @@
             </div>
             <div class="btn-group  ">
                 
-                <button class="btn-nav"  >
+                <button onclick="getAnswer('ALL')"  class="btn-nav"  >
                     ALL
                 </button>
-                <button class="btn-nav"  >
+                <button onclick="getAnswer('DPO')" class="btn-nav"  >
                     DPO
                 </button>
-                <button class="btn-nav"  >
+                <button  onclick="getAnswer('PPA')" class="btn-nav">
                     PPA
                 </button>
-                <button class="btn-nav"  >
+                <button onclick="getAnswer('BANKOM')" class="btn-nav">
                     BANKOM
                 </button>
-                <button class="btn-nav"  >
+                <button onclick="getAnswer('ORMAS')" class="btn-nav">
                     ORMAS
                 </button>
-                <button class="btn-nav">
+                <button onclick="getAnswer('3 PILAR')" class="btn-nav">
                     3 PILAR
                 </button>
-                <button class="btn-nav"  >
+                <button onclick="getAnswer('PEMILU')" class="btn-nav">
                     PEMILU
                 </button>
-                <button class="btn-nav"  >
+                <button onclick="getAnswer('POLSUS')" class="btn-nav">
                     POLSUS
+                </button>
+                <button onclick="getAnswer('OJOL')" class="btn-nav">
+                    OJOL
+                </button>
+                <button onclick="getAnswer('ZEBRA TRAINING CENTER')" class="btn-nav">
+                    ZEBRA TRAINING CENTER
                 </button>
 
             </div>
         </nav>
     </div>
-    <style>
-    .navbar>.container-fluid {
-        display: flex;
-        flex-wrap: nowrap;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .global-navbar {
-    /* background-color: #33333366; */
-    background: #1b1b1b;
-    }
-
-    .btn-group-nav {
-        display: flex;
-        justify-content: center;
-    }
-
-    .btn-group {
-        gap: 8px;
-        display: flex;
-        overflow-x: auto;
-        padding: 0 12px;
-        margin-top: 1rem;
-    }
-    .btn-nav {
-        border-radius: 10px;
-        min-width: 5.2rem;
-        background-color: #333333;
-        color: white;
-        border: none;
-        padding: 0.5rem 0;
-    }
-    .btn-nav:hover {
-        background-color: #fedf2c;
-    }
-    .btn-outline-success {
-        color: #fedf2c;
-        border: 1px solid #fedf2c;
-    }
-    .btn-outline-success:hover {
-        background-color: #fedf2c;
-    }
-    .btn-outline-success:active {
-        background-color: #fedf2c !important    ;
-    }
-
-    .img span {
-        margin-left: 1rem;
-        line-height: 20px;
-        font-weight: 600;
-    }
-    .img-responsive {
-        max-width: 5rem; 
-        height: auto;
-
-    }
+        <script>
 
 
-    @media only screen and (max-width: 600px) {
-        .img span {
-            display: none;
-        }
-        .nav-up{
-            padding: 0 1rem;
-        }
-        .topics {
-            padding: 0 10%;
-        }
-    }
 
-    @media only screen and (min-width: 768px) {
-        .nav-up{
-            padding: 0 2rem ;
-        }
-    }
-
-    @media only screen and (min-width: 992px) {
-        .nav-up{
-            padding: 0 4rem ;
-        }
-    }
-    </style>
+        </script>
 </section>
